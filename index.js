f4cdc70cd1404d449be28d2c6dc2506f9f16f732
Zdravko Branzov <Zdravko.Branzov@progress.com>
2018-03-09 11:46:22 +0200

    add build app check

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const https = require('https');
 const fs = require('fs');
+const child_process = require("child_process");
+const parseArgs = require('minimist');
+const rimraf = require('rimraf');
 
-let getPlugins = new Promise(
+const possibleFiles = ["AndroidManifest.xml", "java", "jniLibs", "res", "assets"];
+const tns = "../node_modules/.bin/tns";
+
+const getPlugins = new Promise(
     function getPlugins(resolve, reject) {
 
         const options = {
@@ -14,8 +20,6 @@ let getPlugins = new Promise(
         };
         let wholeData = "";
         https.get(options, (res) => {
-            console.log('statusCode:', res.statusCode);
-            console.log('headers:', res.headers);
 
             res.on('data', (data) => {
                 wholeData = wholeData.concat(data);
@@ -32,11 +36,111 @@ let getPlugins = new Promise(
         });
     });
 
-let testPlugins = function () {
+const testPlugins = function () {
     getPlugins
         .then(function (plugins) {
-            console.log(plugins[1]);
+            const argv = parseArgs(process.argv, opts = {})
+            const fromIndex = argv.from || 0;
+            const toIndex = argv.to > plugins.length ? plugins.length : argv.to;
+            console.log("Testing plugins from " + fromIndex + " to " + toIndex);
+            for (let index = fromIndex; index < toIndex; index++) {
+
+                let pluginName = plugins[index].name;
+                installPlugin(pluginName);
+                buildApp(index, pluginName);
+                checkForAarFile(pluginName);
+                removePlugin(pluginName);
+                removePlatforms();
+            }
         })
-}
+};
+
+const findOne = function (actualFiles, searchedFiles) {
+    return searchedFiles.some(function (item) {
+        if (actualFiles.indexOf(item) >= 0) {
+            console.log("Found " + item);
+            return true;
+        } else {
+            return false;
+        }
+    });
+};
+
+const log = function (data, fileName) {
+    var file = fileName || "log.txt";
+    console.log(data);
+    try {
+        fs.appendFileSync(file, data + "\n");
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const installPlugin = function (pluginName) {
+    try {
+        child_process.execSync("cd demo &&" + tns + " plugin add " + pluginName);
+        console.log("\nInstalled " + pluginName);
+    } catch (e) {
+        const error = e.stderr ? e.stderr.toString() : e;
+        log(error);
+    }
+};
+
+const buildApp = function (index, pluginName) {
+    try {
+        child_process.execSync("cd demo && " + tns + " build android");
+        console.log("App built");
+    } catch (e) {
+        log(">>> Failed building " + pluginName + " at index " + index);
+        const error = e.stderr ? e.stderr.toString() : e;
+        log(error);
+    }
+};
+
+const removePlugin = function (pluginName) {
+    try {
+        child_process.execSync("cd demo &&" + tns + " plugin remove " + pluginName);
+        console.log("Removed " + pluginName);
+    } catch (e) {
+        const error = e.stderr ? e.stderr.toString() : e;
+        log(error);
+    }
+};
+
+const checkForAarFile = function (pluginName) {
+    try {
+        let androidFiles = [];
+        const path = "./demo/node_modules/" + pluginName + "/platforms/android";
+        if (fs.existsSync(path)) {
+            androidFiles = fs.readdirSync(path);
+        }
+        const shouldHaveAar = findOne(androidFiles, possibleFiles);
+        let hasAarFile = false;
+        if (shouldHaveAar) {
+            const pluginAar = pluginName.replace("-", "_") + ".aar";
+            hasAarFile = androidFiles.includes(pluginAar);
+
+            if (!hasAarFile) {
+                const message = ">>> " + pluginName + " has no Aar file!!!";
+                log(message);
+            } else {
+                console.log(pluginName + " has .aar file");
+            }
+        }
+    } catch (e) {
+        const error = e.stderr ? e.stderr.toString() : e;
+        log(error);
+    }
+};
+
+const removePlatforms = function () {
+    try {
+        rimraf.sync('demo/platforms');
+        console.log("Platforms folder removed");
+    } catch (e) {
+        const error = e.stderr ? e.stderr.toString() : e;
+        log(error);
+    }
+};
 
 testPlugins();
